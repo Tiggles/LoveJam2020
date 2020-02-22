a77bbbd39c6c75c15b2685ffc493a5fae613debd
Jonas Malte Hinchely <jonas@hinchely.dk>
2020-02-22 15:21:10 +0100

    Add different loops for different gamestates

--- src/Main.lua
+++ src/Main.lua
@@ -4,7 +4,18 @@ local initial_width = 800
 local points = 9999
 local max_points = 999
 local frequency_range = 1037 - 887
-local current_frequency = 123
+local current_frequency = 0
+local frequency_offset = 887
+local frequency_max = 1037
+local start_rect = {
+    width = 80,
+    height = 30,
+    x = initial_width / 2 - 40,
+    y = initial_height - 40
+}
+
+local update_loop
+local draw_loop
 
 local dad = {
     last_candy = nil,
@@ -31,31 +42,44 @@ local scale_y = 1
 local debug = true
 
 function love.load()
+
+    update_loop = Menu_update
+    draw_loop = Menu_draw
     love.window.setTitle("Road trip")
     love.window.setMode(initial_width, initial_height, {
         vsync = 1,
         resizable  = true
     })
 
-    --[[ 
+    --[[
         TODO: Add music
         TODO: Add static
     ]]
 end
 
---[[
-    TODO: Start screen
-]]
-
---[[
-    TODO: Fail state
-]]
-
 function love.update(delta)
     if love.keyboard.isDown("escape") then
         love.event.quit()
     end
+    update_loop(delta)
+end
+
+function Fail_loop(delta)
+    -- TODO
+end
+
+function Game_loop(delta)
     Set_points(-delta * 10)
+end
+
+function Menu_update(delta)
+    if love.mouse.isDown(1) then
+        local x, y = love.mouse.getPosition()
+        if Is_inside({x = x, y = y}, start_rect) then
+            update_loop = Game_loop
+            draw_loop = Game_draw
+        end
+    end
 end
 
 function love.resize(width, height)
@@ -64,12 +88,7 @@ function love.resize(width, height)
 end
 
 function love.draw()
-    if debug then
-        love.graphics.print(math.floor(points), 5, 5)
-        love.graphics.print(frequency_range, 15, 10)
-        Draw_outline(chocolate_collision_box)
-        Draw_outline(licorice_collision_box)
-    end
+    draw_loop()
 end
 
 function Draw_outline(box)
@@ -94,4 +113,25 @@ end
 
 function Set_points(extra_points)
     points = math.max(math.min(points + extra_points, max_points), -1)
+end
+
+function Is_inside(vec, rect)
+    return vec.x > rect.x and vec.x < rect.x + rect.width and vec.y > rect.y and vec.y < rect.y + rect.height
+end
+
+function Menu_draw()
+    Draw_filled(start_rect)
+end
+
+function Game_draw()
+    if debug then
+        love.graphics.print(math.floor(points), 5, 5)
+        love.graphics.print(frequency_range, 15, 10)
+        Draw_outline(chocolate_collision_box)
+        Draw_outline(licorice_collision_box)
+    end
+end
+
+function Fail_draw()
+
 end
